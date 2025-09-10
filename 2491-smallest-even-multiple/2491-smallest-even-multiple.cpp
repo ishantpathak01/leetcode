20ea1151e1c22ed4de369473946ca75d87f515f5
Ishant Pathak <ishantpathak25@gmail.com>
2025-09-10 20:07:04 +0530

    Time: 0 ms (100%), Space: 8 MB (27.84%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int smallestEvenMultiple(int n) {
         if(n%2==0){
-            return n*2/2;
+            return n;
         }
         else{
             return n*2;
