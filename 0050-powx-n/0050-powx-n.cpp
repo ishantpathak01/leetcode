b3d80a6618036f85994d1283e381c5bf9aceb182
Ishant Pathak <ishantpathak25@gmail.com>
2025-09-27 18:54:22 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.16%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,19 @@
-
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        long long exp = n;       
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+
+        double result = 1;
+        while (exp>0) {
+            if (exp % 2==1) result *= x;
+            x *= x;
+            exp /= 2;
+        }
+
+        return result;
     }
 };
