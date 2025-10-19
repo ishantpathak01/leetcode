4f4990fea43ae0506f6c0ef8cf773676d9473547
Ishant Pathak <ishantpathak25@gmail.com>
2025-10-19 21:24:32 +0530

    Time: 3 ms (23.08%), Space: 25.1 MB (15.38%) - LeetHub

---
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        int i = 1;
+        while (true) {
+            if (i % k == 0 && s.find(i) == s.end()) return i;
+            i++;
+        }
+    }
+};
