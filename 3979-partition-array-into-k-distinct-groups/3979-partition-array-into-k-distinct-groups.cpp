01888ade13ab353d9043d4cd68ffce63310b2a43
Ishant Pathak <ishantpathak25@gmail.com>
2025-10-22 00:22:01 +0530

    Time: 95 ms (23.22%), Space: 286.7 MB (22.36%) - LeetHub

--- 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.cpp
+++ 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool partitionArray(vector<int>& nums, int k) {
-        if(nums.size()%k!=0){
-            return false;
-        }
+       if(nums.size()%k!=0){
+        return false;
+       }
         map<int,int>freq;
         for(int i=0;i<nums.size();i++){
             freq[nums[i]]++;
