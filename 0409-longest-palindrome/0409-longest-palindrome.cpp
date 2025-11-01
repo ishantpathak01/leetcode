31289dd67902d1ac6ba53bf0cf1036bfa1954389
Ishant Pathak <ishantpathak25@gmail.com>
2025-11-01 23:35:03 +0530

    Time: 0 ms (100%), Space: 9 MB (94.25%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         int oddFrequencyCount = 0;
-        unordered_map<char, int> charFrequency;
+        map<char, int> charFrequency;
         for (char ch : s) {
             charFrequency[ch]++;
             if (charFrequency[ch] % 2 == 1)
