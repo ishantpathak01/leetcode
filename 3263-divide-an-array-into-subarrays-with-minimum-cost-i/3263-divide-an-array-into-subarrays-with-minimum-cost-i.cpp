7f81f9ec5a79de15da76ed51dc09fc982f4b058e
Ishant Pathak <ishantpathak25@gmail.com>
2025-11-02 00:51:27 +0530

    Time: 0 ms (100%), Space: 30.4 MB (70.21%) - LeetHub

---
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/3263-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+       int s=nums[0];
+       sort(nums.begin()+1,nums.end());
+      s=s+nums[1]+nums[2];
+      return s;
+    }
+};
