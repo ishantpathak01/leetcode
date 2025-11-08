9cb65be65cd50b5af56eb474d6ddf72bfe73a424
Ishant Pathak <ishantpathak25@gmail.com>
2025-11-09 00:43:47 +0530

    Time: 0 ms (100%), Space: 31.6 MB (0%) - LeetHub

---
+++ 4116-minimum-moves-to-equal-array-elements-iii/4116-minimum-moves-to-equal-array-elements-iii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum =sum+ nums[nums.size() - 1] - nums[i];
+        }
+        return sum;
+    }
+};
