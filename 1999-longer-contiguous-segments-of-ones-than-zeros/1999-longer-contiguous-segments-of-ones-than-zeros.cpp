e6aadbb82d02a17dfd706cd542220fc43b060007
Ishant Pathak <ishantpathak25@gmail.com>
2025-11-15 00:14:30 +0530

    Time: 0 ms (100%), Space: 8.2 MB (70.45%) - LeetHub

---
+++ 1999-longer-contiguous-segments-of-ones-than-zeros/1999-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkZeroOnes(string s) {
+        int max1 = 0, max0 = 0;
+        int count1 = 0, count0 = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '1') {
+                count1++;
+                count0 = 0;
+            } else {
+                count0++;
+                count1 = 0;
+            }
+            max1 = max(max1, count1);
+            max0 = max(max0, count0);
+        }
+        return max1 > max0;
+    }
+};
