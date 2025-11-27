d2a1cc9535b89565936862a8d08dbb7f67b346c3
Ishant Pathak <ishantpathak25@gmail.com>
2025-11-27 09:28:19 +0530

    Time: 0 ms (100%), Space: 8.3 MB (78.21%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int countA = 0;
+        int countL = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'A') {
+                countA++;
+                if (countA >= 2) return false; 
+            }
+            if (s[i] == 'L') {
+                countL++;                   
+                if (countL >= 3) return false;
+            } 
+            else {
+                countL = 0;    
+            }
+        }
+        return true; 
+    }
+};
