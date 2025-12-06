0c8e8e5eb30e7cced9d10e08f891d12fc2db9f8d
Ishant Pathak <ishantpathak25@gmail.com>
2025-12-06 15:19:10 +0530

    Time: 7 ms (6.89%), Space: 25.1 MB (65.4%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        map<int, int> freq;
+        
+        for (int x : nums) {
+            freq[x]++;
+            if (freq[x] > 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
