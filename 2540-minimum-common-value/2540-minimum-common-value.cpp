3f9129cbe3843eb388f6bd7f4831e82d52f0667a
Ishant Pathak <ishantpathak25@gmail.com>
2025-12-17 21:18:17 +0530

    Time: 100 ms (5.01%), Space: 79.5 MB (5.02%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        map<int, int> freq;
+        for (int i = 0; i < nums1.size(); i++) {
+            freq[nums1[i]]++;
+        }
+        for (int j = 0; j < nums2.size(); j++) {
+            if (freq[nums2[j]] > 0) {
+                return nums2[j];
+            }
+        }
+        return -1;
+    }
+};
