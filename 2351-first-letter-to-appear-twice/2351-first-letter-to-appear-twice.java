091e0581f34c17d46fdbca31703d1fa424f57264
Ishant Pathak <ishantpathak25@gmail.com>
2026-03-03 17:12:11 +0530

    Time: 0 ms (100%), Space: 43 MB (32.11%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i =0;i<s.length();i++){
+            char ch =s.charAt(i);
+            map.put(ch,map.getOrDefault(ch, 0)+1);
+            if(map.get(ch) ==2){
+                return ch;
+            }
+        }
+        return ' ';
+        
+    }
+}
