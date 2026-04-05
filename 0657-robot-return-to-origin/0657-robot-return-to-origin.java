7d59067434de62cef560517a9fcc4d2c39b6a53b
Ishant Pathak <ishantpathak25@gmail.com>
2026-04-06 01:56:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,5 +1,27 @@
 class Solution {
-    public boolean judgeCircle(String moves) {
-        
+public:
+    bool judgeCircle(string moves) {
+        int up=0;
+        int down=0;
+        int left =0;
+        int right =0;
+        for(int i=0;i<=moves.size();i++){
+            if(moves[i]=='U'){
+            up++;
+        }
+        else if(moves[i]=='R'){
+        right++;
+        }
+        else if(moves[i]=='L'){
+        left++;
+        }
+        else if(moves[i]=='D'){
+       down++;
+        }
+        }
+         if(up==down && left==right){
+            return true;
+        }
+        return false;
     }
-}
+};
