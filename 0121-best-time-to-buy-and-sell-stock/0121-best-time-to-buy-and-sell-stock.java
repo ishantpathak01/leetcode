722c4af7e59014f91e6e871cbcdabdd7235b1b73
Ishant Pathak <ishantpathak25@gmail.com>
2026-04-22 11:07:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+          int buy = prices[0];
+        int maxprofit = 0;
+        for(int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - buy;
+            maxprofit = Math.max(maxprofit, profit);
+            if(prices[i] < buy) {
+                buy = prices[i];
+        }
+        }
+        return maxprofit;
+    }
+}
