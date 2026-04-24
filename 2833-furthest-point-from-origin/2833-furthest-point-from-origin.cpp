de7a15095371a53336429de9e3a9bf254ee0a254
Ishant Pathak <ishantpathak25@gmail.com>
2026-04-24 19:33:59 +0530

    Time: 0 ms (100%), Space: 9.1 MB (30.54%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int left=0;
+        int right=0;
+        int space=0;
+        for(int i=0;i<moves.size();i++){
+            if(moves[i]=='L'){
+                left++;
+            }
+            else if(moves[i]=='R'){
+                right++;
+            }
+            else{
+                space++;
+            }
+        }
+        return abs(right-left)+space;
+    }
+};
