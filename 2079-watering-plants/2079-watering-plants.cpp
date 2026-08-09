cbc0833f3d9fd1b6b13bcc64df997902254ac809
Ishant Pathak <ishantpathak25@gmail.com>
2026-08-09 14:05:37 +0530

    Time: 0 ms (100%), Space: 11.9 MB (56.41%) - LeetHub

---
+++ 2079-watering-plants/2079-watering-plants.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int wateringPlants(vector<int>& plants, int capacity) {
+        int steps = 0;
+        int val = capacity;
+        for (int i = 0; i < plants.size(); i++) {
+            if (val < plants[i]) {
+                steps += 2 * i;   
+                val = capacity;
+            }
+
+            val -= plants[i];
+            steps++;  
+        }
+
+        return steps;
+    }
+};
