47502c6e8a1b7f44ad850207530fc8a4416688fd
Ishant Pathak <ishantpathak25@gmail.com>
2026-08-22 12:18:27 +0530

    Time: 847 ms (9.15%), Space: 9.3 MB (91.88%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        int maxLength = 0;
-        int index = 0;
-        auto checkpalindrome= [&](int i, int j) {
-            while (i < j) {
-                if (s[i] != s[j]) {
-                    return false;
-                }
-                i++;
-                j--;
+
+    bool checkpalindrome(const string& s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false;
             }
-            return true;
-        };
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (checkpalindrome(i, j)) {
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    string longestPalindrome(string s) {
+        int maxlength = 0;
+        int index = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            for (int j = i; j < s.size(); j++) {
+
+                if (checkpalindrome(s, i, j)) {
+
                     int length = j - i + 1;
-                    if (length > maxLength) {
-                        maxLength = length;
+
+                    if (length > maxlength) {
+                        maxlength = length;
                         index = i;
                     }
                 }
             }
         }
-        return s.substr(index, maxLength);
+
+        return s.substr(index, maxlength);
     }
 };
