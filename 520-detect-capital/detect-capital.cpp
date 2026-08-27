10e3dfa86e4c6e569a3852c09f995656f0957d58
Ishant Pathak <ishantpathak25@gmail.com>
2026-08-27 22:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (50.63%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(int i=0; i<word.size();i++){
+            if(isupper(word[i])){
+                count++;
+            }
+            }
+             if(count==word.size()){
+                return true;
+        }
+         if(count==0) return true;
+         if (count == 1 && isupper(word[0])) return true;
+
+
+        return false;
+    }
+};
