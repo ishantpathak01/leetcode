c615f3a88bbc2e301aac202068e4a3edff31d7f5
Ishant Pathak <ishantpathak25@gmail.com>
2026-09-05 00:13:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (61.03%) - LeetSync

---
+++ 1095-two-city-scheduling/two-city-scheduling.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        vector<int>result;
+        int ans=0;
+        for(int i=0;i<costs.size();i++){
+             ans+=costs[i][0];
+            result.push_back(costs[i][1]-costs[i][0]);
+        }
+        sort(result.begin(),result.end());
+        for(int i=0;i<costs.size()/2;i++){
+            ans+=result[i];
+        }
+        return ans;
+    }
+};
