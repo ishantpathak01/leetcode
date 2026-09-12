4e0a3f1c13afddf8943b673477ce1648917d8c13
Ishant Pathak <ishantpathak25@gmail.com>
2026-09-12 21:46:14 +0530

    Time: 19 ms (24.53%) | Memory: 34.5 MB (14.68%) - LeetSync

---
+++ 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        map<int, vector<int>> freq;
+        for (int i = 0; i < nums.size(); i++) {
+            freq[nums[i]].push_back(i);
+        }
+        int ans = 0;
+        for (auto it : freq) {
+            if (it.second.size() == 3) {
+                int i1 = it.second[0];
+                int i2 = it.second[1];
+                int i3 = it.second[2];
+                if (i2 - i1 == i3 - i2) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
